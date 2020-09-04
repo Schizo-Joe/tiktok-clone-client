b7d80ab02608b0a75955f38d7afd0e9de15feaf6
korathbasil <korathbasil@live.com>
2020-09-04 19:35:15 +0530

    added main body and video container with snapping functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
+import Video from "./components/Video/Video";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <div className="app__videoContainer">
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Video/Video.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Video.css";
+
+function Video() {
+  return (
+    <div className="video">
+      <video src=""></video>
+      {/* <VideoFooter />
+      <VideoSidebar /> */}
+    </div>
+  );
+}
+
+export default Video;
